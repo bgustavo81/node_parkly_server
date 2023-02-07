f9df638569d0a88681715cb17b133aa721b90332
bgustavo81 <37317094+bgustavo81@users.noreply.github.com>
2023-02-07 09:37:20 -0500

    Delete client directory

--- client/src/apis/nationalParks.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-export default axios.create({
-    baseURL: `https://developer.nps.gov/api/v1/parks?&api_key=${process.env.REACT_APP_NPS_KEY}`,
-});
--- client/src/history.js
+++
@@ -1,7 +0,0 @@
-// import createHistory from 'history/createBrowserHistory';
-
-// export default createHistory();
-
-// One other possible implementation of the history object
-import { createBrowserHistory } from 'history'; 
-export default createBrowserHistory();
